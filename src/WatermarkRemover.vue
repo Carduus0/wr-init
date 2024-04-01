987dfd19397d88d6f43b02d5b40fb2e04ac1739e
Carduus0 <s.carduus@gmail.com>
2024-04-01 13:43:11 +0300

    container

--- src/WatermarkRemover.vue
+++ src/WatermarkRemover.vue
@@ -6,6 +6,32 @@
 <script>
 alert("I`m here!");
 document.body.style.backgroundColor = "#b9f7c3";
+function embedVueComponent() {
+  // контейнер для нашего приложения
+  const appContainer = document.createElement("div");
+  const closeButton = document.createElement("button");
+  appContainer.id = "watermark-remover-container";
+  appContainer.style.position = "relative";
+  appContainer.style.position = "fixed";
+  appContainer.style.top = "0";
+  appContainer.style.right = "0";
+  appContainer.style.width = "440px";
+  appContainer.style.height = "580px";
+  appContainer.style.border = "2px red dashed";
+  appContainer.style.backgroundColor = "transparent";
+
+  appContainer.style.zIndex = "2147483647"; // Максимально возможное значение для z-index
+  closeButton.style.position = "absolute";
+  closeButton.style.top = "5px";
+  closeButton.style.right = "5px";
+  appContainer.innerText = "Watermark Remover Loading...";
+  closeButton.onclick = () => {
+    windowContainer.remove();
+  };
+  document.body.appendChild(appContainer);
+}
+// Выполняем встраивание
+embedVueComponent();
 // import { ref } from "vue";
 
 // const show = ref(true);
@@ -55,5 +81,10 @@ document.body.style.backgroundColor = "#b9f7c3";
   height: 800px;
   background-color: red;
   border: 2px dashed;
+  /*----------*/
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 2147483647;
 }
 </style>
